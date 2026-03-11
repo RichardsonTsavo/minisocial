06ee6605e6180ee328950e455094435a8497a68a
Richardson Tsavo <contatoricdeveloper@gmail.com>
2026-03-11 11:35:46 -0300

    refactor: :fire: initialize modular architecture & configure android permissions
    - Modular architecture setup using Flutter Modular and Slidy
    - Initial project dependencies
    - Android permissions configuration

---
+++ lib/app/app_module.dart
@@ -0,0 +1,13 @@
+import 'package:flutter_modular/flutter_modular.dart';
+
+import 'modules/home/home_module.dart';
+
+class AppModule extends Module {
+  @override
+  void binds(Injector i) {}
+
+  @override
+  void routes(RouteManager r) {
+    r.module('/', module: HomeModule(), transition: TransitionType.fadeIn);
+  }
+}
